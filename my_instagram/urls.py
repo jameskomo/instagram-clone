c0968a1916ab705fd13de7ead7ea798300b09443
jameskomo <james.komoh@gmail.com>
2019-05-17 15:46:38 +0300

    Bootstrap installed, added to INSTALLED APPS, Navbar templated added and included in base template, static files loaded in base template and settings modified in settings

--- my_instagram/urls.py
+++ my_instagram/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns=[
-    path('', views.home, name='instagram-home'),
+    path('', views.home, name='my_instagram-home'),
+    path('navbar/', views.navbar, name='my_instagram-navbar'),
 ]
--- my_instagram/views.py
+++ my_instagram/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, ('my_instagram/base.html'))
+
+def navbar(request):
+    return render(request, ('my_instagram/navbar.html'))
