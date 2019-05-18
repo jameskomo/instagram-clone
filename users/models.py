32d9f66052f83746be057fe4a805080503768e9f
jameskomo <james.komoh@gmail.com>
2019-05-18 13:58:58 +0300

    Automatic profile photo upload for users with no progile photos done through Django signal. Signal.py, post_save, receiver and user & profile models imported. Create and Save signal methods created and ready method created in apps.py to initialize it

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user=models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_image=models.ImageField(default="default.jpeg", upload_to='profile_pics')
+    profile_image=models.ImageField(default="profile_pics/default.jpeg", upload_to='profile_pics')
 
     def __str__(self):
         return f'{self.user.username} Profile'
